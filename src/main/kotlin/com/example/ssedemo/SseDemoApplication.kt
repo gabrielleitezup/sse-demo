4c5fa5d01b1a1bec17d7e70d6e44a9085217fb0d
Gabriel Leite <gabriel.leite@zup.com.br>
2020-03-31 01:33:50 -0300

    Renamed application

--- src/main/kotlin/com/example/ssedemo/SseDemoApplication.kt
+++ src/main/kotlin/com/example/ssedemo/SseDemoApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class WebsocketApplication
+class SseDemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<WebsocketApplication>(*args)
+	runApplication<SseDemoApplication>(*args)
 }
